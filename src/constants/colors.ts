17432a928e73d3770fe464a628f82d241c26d37c
Jackson Kovarik <jkovarik@umich.edu>
2026-02-05 21:14:44 -0500

    Add more colors to the global app colors.

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,7 +1,17 @@
 export const Colors = {
     primary: '#FF8820',
-    // secondary: '#34C759',
-    // background: '#FFFFFF',
-    // textPrimary: '#000000',
-    textSecondary: '#828282',
+    primaryLight: '#FFE6C4',
+
+    background: '#F3F4F6',
+
+    success: '#0BC591',
+    successLight: 'rgba(11,197,145,.25)',
+
+    danger: '#FF4040',
+    dangerLight: 'rgba(255,96,96,.25)',
+    
+    darkBlue: '#0B1221',
+    textSecondary: '#9D9D9D',
+    textSecondaryDark: '#828282',
+    textSecondaryLight: '#c4c4c4',
 }
