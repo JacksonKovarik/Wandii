5455db2fcd6d217e14d3b3434e068a24b63afa31
Jackson Kovarik <jkovarik@umich.edu>
2026-02-19 12:27:41 -0500

    Create a Utility class to handle linking in and out of the app as well as update stay to link to Maps.

--- src/app/(trip-info)/[tripId]/(plan)/stays.jsx
+++ src/app/(trip-info)/[tripId]/(plan)/stays.jsx
@@ -1,7 +1,9 @@
 import ReusableTabBar from "@/src/components/reusableTabBar";
 import { Colors } from "@/src/constants/colors";
+import { openAddressInMaps } from "@/src/utils/LinkingUtils";
 import { MaterialIcons } from "@expo/vector-icons";
 import { BlurView } from "expo-blur";
+import * as Clipboard from 'expo-clipboard';
 import { useLocalSearchParams } from "expo-router";
 import { ImageBackground, ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { moderateScale } from "react-native-size-matters";
@@ -9,6 +11,11 @@ import { moderateScale } from "react-native-size-matters";
 export default function Stays() {
   const tripId = useLocalSearchParams();
   
+  const copyToClipboard = async ({ textToCopy }) => {
+    await Clipboard.setStringAsync(textToCopy);
+    alert('Text copied to clipboard!');
+  };
+
   const staysData = [
     {
       id: '1',
@@ -35,20 +42,30 @@ export default function Stays() {
 
   const StayCard = ({ stay }) => {
     return (
-      <View style={{ width: '100%', backgroundColor: '#ffffff', borderRadius: 20, marginBottom: 15, overflow: 'hidden' }}>
-        <ImageBackground source={ require("@/assets/images/Kyoto.jpg") } style={{ height: 140 }} />
-        <View style={{ paddingVertical: 20, paddingHorizontal: 15 }}>
-          <Text style={{ fontSize: moderateScale(16), fontWeight: '600', color: Colors.darkBlue, marginBottom: 5 }}>{stay.name}</Text>
+      <View style={styles.cardShadow}>
+        <View style={styles.cardContainer}>
+
+          {/* Stay Image */}
+          <ImageBackground source={ require("@/assets/images/Kyoto.jpg") } style={styles.cardImage} />
+
+          {/* Stay Information */}
+          <View style={styles.cardContent}>
+            <Text style={styles.stayName}>{stay.name}</Text>
           
+          {/* Address */}
           <View style={{ width: '100%', flexDirection: 'row', gap: 5, marginBottom: 10, backgroundColor: Colors.lightGray, padding: 10, borderRadius: 4, alignSelf: 'flex-start', alignItems: 'center', marginTop: 10 }}>
             <MaterialIcons name="location-pin" size={moderateScale(20)} color={Colors.primary} />
             <Text style={{ flex: 1, fontSize: moderateScale(11), color: Colors.gray, fontWeight: '500'}}>{stay.address}</Text>
-            <TouchableOpacity style={{ marginLeft: 'auto' }} onPress={() => console.log('Copy address')} hitSlop={5}>
+            
+            {/* Copy Button */}
+            <TouchableOpacity style={{ marginLeft: 'auto' }} onPress={() => copyToClipboard({ textToCopy: stay.address })} hitSlop={5}>
               <MaterialIcons name="content-copy" size={moderateScale(16)} color={Colors.gray} style={{ marginLeft: 'auto' }} />
             </TouchableOpacity>
           </View>
 
           <View style={{ height: 2, backgroundColor: Colors.lightGray, width: '100%', marginTop: 10 }} />
+
+          {/* Check In/Out */}
           <View style={{ flexDirection: 'row', gap: 5, alignItems: 'center', marginTop: 15 }}>
             <View style={{ flex: 1, gap: 5 }}>
               <Text style={{ fontSize: moderateScale(10), color: Colors.gray, fontWeight: '700' }}>CHECK IN</Text>
@@ -60,20 +77,27 @@ export default function Stays() {
             </View>
           </View>
   
-          <TouchableOpacity style={{ width: '100%', flexDirection: 'row', paddingVertical: 10, backgroundColor: Colors.darkBlue, alignItems: 'center', justifyContent: 'center', gap: 10, borderRadius: 10, alignSelf: 'center', marginTop: 20}} onPress={() => console.log('Get directions')}>
+          {/* Directions Button */}
+          <TouchableOpacity 
+            style={{ width: '100%', flexDirection: 'row', paddingVertical: 10, backgroundColor: Colors.darkBlue, alignItems: 'center', justifyContent: 'center', gap: 10, borderRadius: 10, alignSelf: 'center', marginTop: 20}}
+            onPress={() => { openAddressInMaps(stay.address);}}
+            hitSlop={5}
+          >
             <MaterialIcons name="near-me" size={moderateScale(16)} color="#ffffff" />
             <Text style={{ fontSize: moderateScale(14), color: '#ffffff', fontWeight: '600' }}>Get Directions</Text>
           </TouchableOpacity>
         </View>
 
+        {/* Edit Button top right of card */}
         <BlurView intensity={50} tint="default" style={{ position: 'absolute', top: 12, right: 10, padding: 5, backgroundColor: 'rgba(255,255,255,0.6)', borderRadius: 25, alignItems: 'center', justifyContent: 'center', overflow: 'hidden' }}>
           <TouchableOpacity onPress={() => console.log('Edit stay')} hitSlop={5}>
             <MaterialIcons name="more-vert" size={moderateScale(20)} color={'white'} />
           </TouchableOpacity>
-        </BlurView>
-        
+        </BlurView>  
       </View>
-    );
+      
+      </View>
+    ); 
   };
   
   return (
@@ -130,5 +154,32 @@ const styles = StyleSheet.create({
   newEntryButton: {
     fontSize: moderateScale(14),
     color: Colors.primary,
+  },
+  cardShadow: {
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    marginBottom: 25,
+  },
+  cardContainer: {
+    backgroundColor: '#ffffff',
+    borderRadius: 20,
+    overflow: 'hidden',
+    width: '100%',
+  },
+  cardImage: {
+    height: 140,
+  },
+  cardContent: {
+    paddingVertical: 20,
+    paddingHorizontal: 15,
+  },
+  stayName: {
+    fontSize: moderateScale(16),
+    fontWeight: '600',
+    color: Colors.darkBlue,
+    marginBottom: 5,
   },
 });
