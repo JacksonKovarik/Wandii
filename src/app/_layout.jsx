61100cc715006a5c71b32418a0f61b11f7a73657
Jackson Kovarik <jkovarik@umich.edu>
2026-03-12 15:36:50 -0400

    Add A basic 'Relive Trip' Past trips functionality.
    Added a 'Delete' functionality for existing upcoming trips

--- src/app/_layout.jsx
+++ src/app/_layout.jsx
@@ -1,24 +1,25 @@
 import { getIsLoggedIn } from "@/src/utils/auth";
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar"; // 1. Import StatusBar from expo
 import { Platform, UIManager } from "react-native";
 import { MenuProvider } from "react-native-popup-menu";
 
 if (Platform.OS === 'android' && UIManager.setLayoutAnimationEnabledExperimental) {
   UIManager.setLayoutAnimationEnabledExperimental(true);
 }
-const isLoggedIn = getIsLoggedIn(); // Replace with actual authentication logic
+const isLoggedIn = getIsLoggedIn(); 
 
 export default function RootLayout() {
   return (
     <MenuProvider>
-      <Stack>
+      <StatusBar style="dark" /> 
 
-        {/* Make the root index route the welcome screen */}
+      <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
         
         <Stack.Protected guard={isLoggedIn}>
           <Stack.Screen name="(tabs)"  options={{ headerShown: false }}/>
-          <Stack.Screen name="(add-trips)" options={{ headerShown: false, presentation: "modal", title: "Modal" }}/>
+          <Stack.Screen name="(add-trips)" options={{ headerShown: false, presentation: "modal" }}/>
           <Stack.Screen name="(trip-info)/[tripId]" options={{ headerShown: false }}/>
         </Stack.Protected>
 
@@ -26,7 +27,6 @@ export default function RootLayout() {
           <Stack.Screen name="sign-in" options={{headerShown: false}}/>
           <Stack.Screen name="sign-up" options={{headerShown: false}}/>
         </Stack.Protected>
-        
       </Stack>
     </MenuProvider>
   );
