f7a67b67548fbf3d983c7a6c0091de6dc7517ad8
Shelbablula <inspireme297@gmail.com>
2026-03-30 20:21:41 -0400

    Refactor layout to use Auth and TripDraft providers

--- src/app/_layout.jsx
+++ src/app/_layout.jsx
@@ -1,33 +1,46 @@
-import { getIsLoggedIn } from "@/src/utils/auth";
+import { AuthProvider, useAuth } from "@/src/context/AuthContext";
+import { TripDraftProvider } from "@/src/context/TripDraftContext";
 import { Stack } from "expo-router";
-import { StatusBar } from "expo-status-bar"; // 1. Import StatusBar from expo
-import { Platform, UIManager } from "react-native";
-import { MenuProvider } from "react-native-popup-menu";
+import React from "react";
 
-if (Platform.OS === 'android' && UIManager.setLayoutAnimationEnabledExperimental) {
-  UIManager.setLayoutAnimationEnabledExperimental(true);
+function RootNavigator() {
+  const { user, loading } = useAuth();
+
+  if (loading) return null;
+
+  const isLoggedIn = !!user;
+
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      {/* index route */}
+      <Stack.Screen name="index" />
+
+      <Stack.Protected guard={isLoggedIn}>
+        <Stack.Screen name="(tabs)" />
+
+        <Stack.Screen
+          name="(add-trips)"
+          options={{ presentation: "modal" }}
+        />
+
+        <Stack.Screen name="(trip-info)/[tripId]" />
+        <Stack.Screen name="settings" />
+      </Stack.Protected>
+
+      <Stack.Protected guard={!isLoggedIn}>
+        <Stack.Screen name="sign-in" />
+        <Stack.Screen name="sign-up" />
+      </Stack.Protected>
+    </Stack>
+  );
 }
-const isLoggedIn = getIsLoggedIn(); 
 
 export default function RootLayout() {
   return (
-    <MenuProvider>
-      <StatusBar style="dark" /> 
-
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        
-        <Stack.Protected guard={isLoggedIn}>
-          <Stack.Screen name="(tabs)"  options={{ headerShown: false }}/>
-          <Stack.Screen name="(add-trips)" options={{ headerShown: false, presentation: "modal" }}/>
-          <Stack.Screen name="(trip-info)/[tripId]" options={{ headerShown: false }}/>
-        </Stack.Protected>
-
-        <Stack.Protected guard={!isLoggedIn}>
-          <Stack.Screen name="sign-in" options={{headerShown: false}}/>
-          <Stack.Screen name="sign-up" options={{headerShown: false}}/>
-        </Stack.Protected>
-      </Stack>
-    </MenuProvider>
+    <AuthProvider>
+      <TripDraftProvider>
+        <RootNavigator />
+      </TripDraftProvider>
+    </AuthProvider>
   );
 }
